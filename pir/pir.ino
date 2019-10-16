599411ef3c42ecf791afbe5f1a8eb8bb8fb750d5
Erico de Souza Loewe <ed-lowe@hotmail.com>
2019-10-16 20:11:49 -0300

    fix(pir): clean code and adjust pinmode

--- pir/pir.ino
+++ pir/pir.ino
@@ -1,14 +1,7 @@
-
-
 #include "WiFi.h" // ESP32 WiFi include
-#include "esp_wpa2.h"
-
-#define EAP_IDENTITY "0118340@feevale.br"
-#define EAP_PASSWORD "160996"
 
 const uint16_t port = 9999;
 const char * host = "192.168.137.1";
-// const char* ssid = "eduroam"; // Eduroam SSID
 const char* ssid     = "DESKTOP-LPD8U10 5875";
 const char* password = "deboas123";
 int counter = 0;
@@ -19,7 +12,7 @@ WiFiClient client;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(motionSensorPort, INPUT_PULLUP);
+  pinMode(motionSensorPort, INPUT);
 
   while (!Serial) {
     ; // wait for serial port to connect. Needed for native USB port only
@@ -27,11 +20,16 @@ void setup() {
 
   connect();
   establishContact();
-  attachInterrupt(digitalPinToInterrupt(motionSensorPort), detectsMovement, CHANGE);
+  // attachInterrupt(digitalPinToInterrupt(motionSensorPort), detectsMovement, CHANGE);
 }
 
 void loop() {
-  // sendInfo();
+  sendInfo();
+}
+
+void detectsMovement() {
+  Serial.println("Movimento detectado!!!");
+  sendInfo();
 }
 
 void establishContact() {
@@ -41,13 +39,8 @@ void establishContact() {
     Serial.print("Carregando ");
     Serial.print(count++);
     Serial.print("\n");
-    delay(300);
+    delay(1000);
   }
-}
-
-void detectsMovement() {
-  Serial.println("Movimento detectado!!!");
-  sendInfo();
 }
 
 void connect()//Sub-rotina para verificar a conexao com o host.
@@ -67,6 +60,13 @@ void connect()//Sub-rotina para verificar a conexao com o host.
 }
 
 void sendInfo() {
+  int sensorValue = digitalRead(motionSensorPort);
+  
+  Serial.print("Valor atual: ");
+  Serial.println(sensorValue);
+
+  Serial.println(WiFi.localIP());
+
   if (!client.connect(host, port)) {
     Serial.println("Connection to host failed");
 
@@ -74,38 +74,7 @@ void sendInfo() {
     return;
   }
 
-  int sensorValue = digitalRead(motionSensorPort);
-
-  Serial.print("VALOR ATUAL: ");
-  Serial.print(digitalRead(motionSensorPort));
-  Serial.print("\n");
   Serial.println("Sending sensor value to IP");
   client.print(sensorValue);
   delay(1000);
-}
-
-void connectToWiFiWithEduroam(const char * ssid)
-{
-  Serial.println();
-  Serial.print("Connecting to network: ");
-  Serial.println(ssid);
-
-  WiFi.disconnect(true);  //disconnect form wifi to set new wifi connection
-  WiFi.mode(WIFI_STA); //init wifi mode
-  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide identity
-  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)EAP_IDENTITY, strlen(EAP_IDENTITY)); //provide username --> identity and username is same
-  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)EAP_PASSWORD, strlen(EAP_PASSWORD)); //provide password
-  esp_wpa2_config_t config = WPA2_CONFIG_INIT_DEFAULT(); //set config settings to default
-  esp_wifi_sta_wpa2_ent_enable(&config); //set config settings to enable function
-  WiFi.begin(ssid); //connect to wifi
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-    counter++;
-
-    if (counter >= 60) { //after 30 seconds timeout - reset board
-      ESP.restart();
-    }
-  }
 }
